e1b63963f85fe4fb568ccc7123e551a6ffaff980
Jachdich <jamesedkitching@gmail.com>
2022-05-30 22:49:02 +0100

    made some code more rusty

--- src/star.rs
+++ src/star.rs
@@ -27,41 +27,41 @@ pub struct Star {
 //TODO Find a better function/modify this to be more rusty
 fn k_to_rgb(k: u32) -> u32 {
     let temp: f64 = k as f64 / 100.0;
-    let mut r: i16;
-    let mut g: i16;
-    let mut b: i16;
+    let mut r: f64;
+    let mut g: f64;
+    let mut b: f64;
 
     if temp <= 66.0 {
-    	r = 255;
+    	r = 255.0;
     } else {
-    	r = (temp - 60.0) as i16;
-    	r = (329.698727446 * (r as f64).powf(-0.1332047592)) as i16;
-    	if r < 0 { r = 0; }
-    	if r > 255 { r = 255; }
+    	r = temp - 60.0;
+    	r = 329.698727446 * r.powf(-0.1332047592);
+    	if r < 0.0 { r = 0.0; }
+    	if r > 255.0 { r = 255.0; }
     }
         
     if temp <= 66.0 {
-    	g = temp as i16;
-    	g = (99.4708025861 * f64::ln(g as f64) - 161.1195681661) as i16;
-    	if g < 0 { g = 0; }
-    	if g > 255 { g = 255; }
+    	g = temp;
+    	g = 99.4708025861 * g.ln() - 161.1195681661;
+    	if g < 0.0 { g = 0.0; }
+    	if g > 255.0 { g = 255.0; }
     } else {
-    	g = (temp - 60.0) as i16;
-    	g = (288.1221695283 * (g as f64).powf(-0.0755148492)) as i16;
-    	if g < 0 { g = 0; }
-    	if g > 255 { g = 255; }
+    	g = temp - 60.0;
+    	g = 288.1221695283 * g.powf(-0.0755148492);
+    	if g < 0.0 { g = 0.0; }
+    	if g > 255.0 { g = 255.0; }
     }
     
     if temp >= 66.0 {
-    	b = 255;
+    	b = 255.0;
     } else {
     	if temp <= 19.0 {
-    		b = 0;
+    		b = 0.0;
     	} else {
-    		b = (temp - 10.0) as i16;
-    		b = (138.5177312231 * f64::ln(b as f64) - 305.0447927307) as i16;
-    		if b < 0 { b = 0; }
-    		if b > 255 { b = 255; }
+    		b = temp - 10.0;
+    		b = 138.5177312231 * b.ln() - 305.0447927307;
+    		if b < 0.0 { b = 0.0; }
+    		if b > 255.0 { b = 255.0; }
     	}
     }
 
